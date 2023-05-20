79e44d40acf8cd11ca854a71d9d516e20bcbedc5
Anthonyfl007 <anthonyalexanderfloresleanos.com>
2023-05-20 16:02:03 -0300

    se agrega el proyecto

---
+++ src/git/markdonw/main.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git.markdonw;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author ELENA
+ */
+public class main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         System.out.println("HOLA Git+Markdown");
+        JOptionPane.showMessageDialog(null,"HOLA Git+Markdonw");
+    }
+   
+}
